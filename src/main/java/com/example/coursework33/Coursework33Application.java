f389e64f905ac2284fe172b85c2e12110a822162
Anastasia <nastay.dr2002@gmail.com>
2023-02-22 12:34:49 +0400

    Initial commit

---
+++ src/main/java/com/example/coursework33/Coursework33Application.java
@@ -0,0 +1,13 @@
+package com.example.coursework33;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursework33Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Coursework33Application.class, args);
+    }
+
+}
